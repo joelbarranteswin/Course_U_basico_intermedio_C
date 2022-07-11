2e8bcdec181aaf28036cc02f73993d2376dc0c4c
joelbarrantes <joelbarrantespalacios@gmail.com>
2022-07-11 16:29:38 -0500

    update

--- .Clases/PruebasTurboCEV/Excel/Program.cs
+++ .Clases/PruebasTurboCEV/Excel/Program.cs
@@ -10,6 +10,13 @@ namespace Excel
     {
         static void Main(string[] args)
         {
+            List<string> MyList = new List<string> { "TEXT", "NOTEXT", "test", "notest" };
+            var SList = MyList.FindAll(item => item == item.ToLower());
+
+            foreach (var s in SList)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
